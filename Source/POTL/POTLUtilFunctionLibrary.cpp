4ad3f6110c2e08abff61bb41d62af78c3494fa94
kimFrost <kim@thatworks.dk>
2015-08-03 16:08:37 +0200

    (added) - started on c++ path flood fill

---
+++ Source/POTL/POTLUtilFunctionLibrary.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "POTL.h"
+#include "POTLUtilFunctionLibrary.h"
+
+
+
+// Sets default values
+UPOTLUtilFunctionLibrary::UPOTLUtilFunctionLibrary(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	
+}
+
+
+
+FString UPOTLUtilFunctionLibrary::FindThisFunction(FString ReturnString)
+{
+
+
+
+
+	return ReturnString;
+}
+
+
+TArray<FVector> UPOTLUtilFunctionLibrary::GetHexesWithFloodFill(FVector StartPosition, TArray<FVector> Obstacles, int Range)
+{
+	TArray<FVector> HexesInRange;
+
+
+	return HexesInRange;
+}
---
+++ Source/POTL/POTLUtilFunctionLibrary.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "POTLUtilFunctionLibrary.generated.h"
+
+UCLASS()
+class POTL_API UPOTLUtilFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UPOTLUtilFunctionLibrary(const FObjectInitializer& ObjectInitializer);
+	
+	UFUNCTION(BlueprintCallable, Category="Hex")
+	static FString FindThisFunction(FString ReturnString);
+
+
+	static TArray<FVector> GetHexesWithFloodFill(FVector StartPosition, TArray<FVector> Obstacles, int Range);
+
+};
