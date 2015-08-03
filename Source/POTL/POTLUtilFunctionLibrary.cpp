00fabd6bfc7e8f198f6f7e9745e5c7115970dc2b
kimFrost <kim@thatworks.dk>
2015-08-03 19:46:02 +0200

    ()

--- Source/POTL/POTLUtilFunctionLibrary.cpp
+++ Source/POTL/POTLUtilFunctionLibrary.cpp
@@ -25,7 +25,37 @@ FString UPOTLUtilFunctionLibrary::FindThisFunction(FString ReturnString)
 
 TArray<FVector> UPOTLUtilFunctionLibrary::GetHexesWithFloodFill(FVector StartPosition, TArray<FVector> Obstacles, int Range)
 {
+	struct Fridge
+	{
+		TArray<FVector> CubeCoords;
+	};
+
 	TArray<FVector> HexesInRange;
+	TArray<FVector> VisitedCubeCoords;
+	TArray<FVector> ReachableCubeCoords;
+	TArray<Fridge> Fridges;
+
+	TArray<FVector> CubeDirections;
+
+	CubeDirections.Add({ 1, 0, -1 });
+	CubeDirections.Add({ 1, -1, 0 });
+	CubeDirections.Add({ 0, -1, 1 });
+	CubeDirections.Add({ -1, 0, 1 });
+	CubeDirections.Add({ -1, 1, 0 });
+	CubeDirections.Add({ 0, 1, -1 });
+
+	// Add StartPosition to cubecoords already visited 
+	VisitedCubeCoords.Add(StartPosition);
+
+	Fridge fridge = Fridge{0, 0, 0};
+	Fridges.Add(fridge);
+
+
+
+
+	//auto array = new double[5][5];
+	//int **ary = new int*[];
+
 
 
 	return HexesInRange;
