8a0eea5277a2ddc6c83ffb4485e203849f23e919
Kim Frost <kim@noerd.dk>
2015-08-06 09:28:29 +0200

    (change) - tried flood fill with the old method from blueprint. Not tested yet. Does build though.

--- Source/POTL/POTLUtilFunctionLibrary.cpp
+++ Source/POTL/POTLUtilFunctionLibrary.cpp
@@ -31,10 +31,11 @@ TArray<FVector> UPOTLUtilFunctionLibrary::GetHexesWithFloodFill(FVector StartPos
 	};
 
 	TArray<FVector> VisitedCubeCoords;
-	TArray<FVector> ReachableCubeCoords;
 	//TArray<Fridge> Fridges;
-	FVector Fridges[100][6];
-	
+	//FVector Fridges[100][6];
+	TArray<Fridge> Fridges;
+
+
 	TArray<FVector> CubeDirections;
 
 	CubeDirections.Add({ 1, 0, -1 });
@@ -45,14 +46,66 @@ TArray<FVector> UPOTLUtilFunctionLibrary::GetHexesWithFloodFill(FVector StartPos
 	CubeDirections.Add({ 0, 1, -1 });
 
 	// Add StartPosition to cubecoords already visited 
+	VisitedCubeCoords.Add(StartPosition);
+
+	Fridge fridge;
+	fridge.CubeCoords = Obstacles;
+	Fridges.Add(fridge);
+
+	for (int32 k = 1; k < Range; k++)
+	{
+		Fridge fridge;
+		Fridges.Add(fridge);
+
+		fridge = Fridges[k - 1];
+
+		int arrayLength = fridge.CubeCoords.Num();
+		for (int32 m = 0; m < arrayLength; m++)
+		{
+			FVector CubeCoord = fridge.CubeCoords[m];
+			for (int32 l = 0; l < CubeDirections.Num(); l++)
+			{
+				FVector CubeDirection = CubeDirections[l];
+				FVector CombinedVector = CubeCoord + CubeDirection;
+				bool ExistsInVisited = VisitedCubeCoords.Contains(CombinedVector);
+				bool ExistsInObstacles = Obstacles.Contains(CombinedVector);
+				if (ExistsInVisited || ExistsInObstacles)
+				{
+
+				}
+				else 
+				{
+					VisitedCubeCoords.Add(CombinedVector);
+					Fridges[k].CubeCoords.Add(CombinedVector);
+				}
+			}
+		}
+
+		/*
+		TArray<FVector> CubeCoords = fridge.CubeCoords;
+		for each (CubeCoord var in CubeCoords)
+		{
+
+		}
+		*/
+	}
+
+	
+
+
+
+
+
+	// Add StartPosition to cubecoords already visited 
 	//VisitedCubeCoords.Add(StartPosition);
 	//Fridges[0] = FVector[];
 
 	//Fridge fridge = Fridge{0, 0, 0};
 	//Fridges.Add(fridge);
 
-	Fridges[0][0] = FVector{ StartPosition };
 
+	/*
+	Fridges[0][0] = FVector{ StartPosition };
 
 	for (int32 k = 1; k < Range; k++)
 	{
@@ -65,7 +118,7 @@ TArray<FVector> UPOTLUtilFunctionLibrary::GetHexesWithFloodFill(FVector StartPos
 
 		}
 	}
-
+	*/
 
 
 	//auto array = new double[5][5];
