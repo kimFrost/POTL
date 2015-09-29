32097241c6d68a71b535e79fbc8d4f9384f1becc
Kim Frost Nielsen <kfn@impact.dk>
2015-09-29 09:11:39 +0200

    (fix) - fixed struct garbage collector crash. A actor pointer wasn't initiated

--- Source/POTL/POTLGameInstance.h
+++ Source/POTL/POTLGameInstance.h
@@ -154,8 +154,10 @@ struct FST_Hex
 	//TArray<FST_ConstructLocation> ConstructLocations;
 	FST_ConstructLocation ConstructInfo;
 
-	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex")
-	//AActor* HighlightDecal; // This caused a Pointer Access violation somewhere. Thus crash the game on the first gabage collection.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex")
+	AActor* HighlightDecal; 
+	// This caused a Pointer Access violation somewhere. Thus crash the game on the first gabage collection.
+	// NOTE: It was caused by not having it be initiate in FST_Hex() to be nullptr
 
 	// [2015.09.28-17.57.23:650][ 63]LogWindows: === Critical error: ===
 	//Fatal error : [File:D : \BuildFarm\buildmachine_++depot + UE4 - Releases + 4.9\Engine\Source\Runtime\CoreUObject\Private\UObject\GarbageCollection.cpp] [Line:383]
@@ -181,6 +183,7 @@ struct FST_Hex
 		HexOffsetCoords = {-1, -1};
 		HexIndex = -1;
 		HexNeighborIndexes.Init(-1, 6);
+		HighlightDecal = nullptr;
 	};
 };
 
