2eff3ed2d6d77a7469eb11b4dca002953f627745
Kim Frost Nielsen <kfn@impact.dk>
2017-08-08 15:00:06 +0200

    ()

--- webroot2/src/app/provider/provider.component.ts
+++ webroot2/src/app/provider/provider.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { StructureComponent } from '../structure/structure.component';
 
 @Component({
   selector: 'provider',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./provider.component.css']
 })
 export class ProviderComponent implements OnInit {
+
+  @Input()
+  structure: StructureComponent;
 
   constructor() { }
 
